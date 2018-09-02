f85292cd8f598a89d2a1ab82fc92ebc5689e966f
wll <wll0113@icloud.com>
2018-09-02 15:25:01 +0800

    add function

---
+++ js/minions.js
@@ -0,0 +1,33 @@
+
+	var eyemove = document.getElementById("btn1");
+	eyemove.onclick = function(){
+	// eyemove button
+		alert("dddd");
+	};
+
+
+	var drink = document.getElementById("btn2");
+	drink.onclick = function(){
+	// drink button
+		alert("dddd");
+	};
+
+
+
+	var tongues = document.getElementById("btn3");
+	tongues.onclick = function(){
+	// tongues button
+		alert("dddd");
+	};
+
+
+	var bg = document.getElementById("btn4");
+	bg.onclick = function(){
+	// bg button
+		alert("dddd");
+	};
+
+
+
+
+
